c3cb21315ffbdb24cacb091403e0f07ca976554a
Faadhil Nurdi Gusmadi <l200200028@student.ums.ac.id>
2023-03-29 09:30:39 +0700

    fix old value in edit form

--- resources/views/genres/index.blade.php
+++ resources/views/genres/index.blade.php
@@ -14,7 +14,7 @@
                                         clip-rule="evenodd"></path>
                                 </svg>
                             </div>
-                            <form action="{{ route('admin.permissions.index') }}" method="GET" id="search-form">
+                            <form action="{{ route('admin.genres.index') }}" method="GET" id="search-form">
                                 <input type="text" id="search" name="search"
                                     class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     placeholder="Search for items">
