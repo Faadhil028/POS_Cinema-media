6d1031a60f147ffdda1c9be4be585f15497b8998
Artalis01 <aryoktasulistyo@gmail.com>
2023-04-14 09:20:08 +0700

    FIX: Genres Table Migration Revision

--- database/migrations/2023_04_13_145528_create_genres_table.php
+++ database/migrations/2023_04_13_145528_create_genres_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('genres', function (Blueprint $table) {
             $table->integer('id', true);
-            $table->string('name', 45);
+            $table->string('name', 45)->unique();
             $table->tinyInteger('is_active')->default(1);
         });
     }
