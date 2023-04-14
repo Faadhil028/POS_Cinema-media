7fac53ebf15e2beaf0ba140c499683cbfa46e606
Faadhil Nurdi Gusmadi <l200200028@student.ums.ac.id>
2023-04-14 09:20:54 +0700

    FEAT: add film & genre seeder

---
+++ database/seeders/GenreSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Genre;
+
+class GenreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Genre::create([
+            'name' => 'Action',
+            'is_active' => '1']);
+        Genre::create([
+            'name' => 'Horror',
+            'is_active' => '1']);
+        Genre::create([
+            'name' => 'Drama',
+            'is_active' => '1']);
+        Genre::create([
+            'name' => 'Romance',
+            'is_active' => '1']);
+        Genre::create([
+            'name' => 'Sci-fi',
+            'is_active' => '1']);
+        Genre::create([
+            'name' => 'Comedy',
+            'is_active' => '1']);
+        Genre::create([
+            'name' => 'Fantasy',
+            'is_active' => '0']);
+        Genre::create([
+            'name' => 'Advanture',
+            'is_active' => '1']);
+    }
+}
