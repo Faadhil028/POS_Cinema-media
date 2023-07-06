1eb67ad4357207912053cbbf325ad08d873c9c95
Syaiful Rochman <l200200023@student.ums.ac.id>
2023-07-06 14:40:29 +0700

    Mengubah Dashboard admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,109 +3,45 @@
 @endpush
 <x-admin-layout>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 w-100">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{-- {{ __('Admin Content') }} --}}
-                    {{-- @php
-                        $start_date = \Carbon\Carbon::parse('2023-04-01');
-                        $end_date = \Carbon\Carbon::parse('2023-04-10');
-                    @endphp
 
-                    @while ($start_date->lte($end_date))
-                        <p>{{ $start_date->toDateString() }}</p>
-                        @php
-                            $start_date->addDay();
-                        @endphp
-                    @endwhile --}}
+                    <h1 class="text-3xl font-bold mb-4">Dashboard Bioskop</h1>
+                    <p class="mb-4 text-lg">Selamat datang di dashboard Bioskop SOC Movie. Di sini, Anda dapat mengelola
+                        berbagai informasi terkait bioskop, termasuk jadwal film, penjualan tiket, dan pengelolaan
+                        data
+                        pelanggan.</p>
+                    <p class="mb-4 text-lg">Dashboard ini menyediakan fitur-fitur yang memudahkan Anda dalam mengatur
+                        operasional bioskop. Anda dapat dengan mudah menambah, mengedit, atau menghapus jadwal film,
+                        mengelola harga tiket, serta melihat laporan penjualan.</p>
+                    <p class="mb-4 text-lg">Kami berkomitmen untuk menyediakan pengalaman yang optimal bagi
+                        pengelolaan
+                        bioskop Anda melalui dashboard ini. Jika Anda memiliki pertanyaan atau masalah, jangan ragu
+                        untuk
+                        menghubungi tim dukungan kami.</p>
 
-
-                    <div class="clearfix"></div>
-                    <br />
-
-                    <div class="col-div-3">
-                        <div class="box">
-                            <p>67<br /><span>Customers</span></p>
-
-                        </div>
+                    <div class="border-t border-gray-700 mt-6 pt-6">
+                        <h2 class="text-xl font-semibold mb-4">Fitur Dashboard Bioskop</h2>
+                        <ul class="list-disc list-inside mb-4">
+                            <li class="text-lg">Manajemen Jadwal Film</li>
+                            <li class="text-lg">Pemesanan Tiket </li>
+                            <li class="text-lg">Pengelolaan Data Pelanggan</li>
+                            <li class="text-lg">Laporan Penjualan</li>
+                        </ul>
+                        <p class="text-lg">Dashboard ini memberikan kemudahan dan visualisasi yang intuitif untuk
+                            membantu Anda
+                            memahami operasional bioskop dengan efektif dan efisien.</p>
                     </div>
-                    <div class="col-div-3">
-                        <div class="box">
-                            <p>88<br /><span>Projects</span></p>
-
-                        </div>
-                    </div>
-                    <div class="col-div-3">
-                        <div class="box">
-                            <p>99<br /><span>Orders</span></p>
-
-                        </div>
-                    </div>
-                    <div class="col-div-3">
-                        <div class="box">
-                            <p>78<br /><span>Tasks</span></p>
-
-                        </div>
-                    </div>
-                    <div class="clearfix"></div>
-                    <br /><br />
-                    <div class="col-div-8">
-                        <div class="box-8">
-                            <div class="content-box">
-                                <p>Top Selling Projects </p>
-                                <br />
-                                <table>
-                                    <tr>
-                                        <th>Company</th>
-                                        <th>Contact</th>
-                                        <th>Country</th>
-                                    </tr>
-                                    <tr>
-                                        <td>Alfreds Futterkiste</td>
-                                        <td>Maria Anders</td>
-                                        <td>Germany</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Centro comercial Moctezuma</td>
-                                        <td>Francisco Chang</td>
-                                        <td>Mexico</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Ernst Handel</td>
-                                        <td>Roland Mendel</td>
-                                        <td>Austria</td>
-                                    </tr>
-
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-div-4">
-                        <div class="box-4">
-                            <div class="content-box">
-                                <p>Total Sale </p>
-
-                                <div class="circle-wrap">
-                                    <div class="circle">
-                                        <div class="mask full">
-                                            <div class="fill"></div>
-                                        </div>
-                                        <div class="mask half">
-                                            <div class="fill"></div>
-                                        </div>
-                                        <div class="inside-circle"> 99% </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="clearfix"></div>
-
-
                 </div>
-            </div>
-        </div>
-    </div>
+
+
+
+
+                <div class="clearfix"></div>
+                <br />
+
+                <div class="clearfix"></div>
+
+
 </x-admin-layout>
